d565e4424993cd7581cc842cae11d6ccf9df9107
sarang <sarang>
2022-02-23 22:27:36 +0530

    ************************ Rendering flows**************** 1 *ng-if directive 2 *ngFor and iterating collections 3 ngClass and className directive bindings 4 ngStyle and style directive  bindings 5 Pipes for data transformation 6 safe-navigation
    All code and theory updated and txt file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,83 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string;
+  age: number;
+}
+
+interface Passengers {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+  checkInDate: number | null;
+  children: Child[] | null;
+}
+
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
   template: `
-    <div class="app">
-      <button (click)="handleClick(username.value)">Get value</button>
-      <input type="text" #username />
-      <div>{{ name }}</div>
-    </div>
+    <h3>Airlines passengers</h3>
+    <ul>
+      <li *ngFor="let passenger of passengers; let i = index">
+        <span class="status" [class.checked-in]="passenger.checkedIn"></span>
+        {{ i }} {{ passenger.fullname }}
+
+        <div class="date">
+          {{
+            passenger.checkInDate
+              ? (passenger.checkInDate | date: 'yMMMMd' | uppercase)
+              : 'not checked in'
+          }}
+        </div>
+
+        <div class="children">
+          {{ passenger.children?.length || 0 }}
+        </div>
+      </li>
+    </ul>
   `,
   // templateUrl: './app.component.html',
 })
 export class AppComponent {
-  name: string = 'sarangs';
-  handleClick(value: string) {
-    console.log(value);
-    this.name = value;
-  }
+  passengers: Passengers[] = [
+    {
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null,
+    },
+    {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+      checkInDate: null,
+      children: [
+        { name: 'Ted', age: 12 },
+        { name: 'Chloe', age: 7 },
+      ],
+    },
+    {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+      checkInDate: 1491606000000,
+      children: null,
+    },
+    {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }],
+    },
+    {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+      checkInDate: null,
+      children: null,
+    },
+  ];
 }
